613fe3c1da6894b35f39af1f1aec7b53be61c535
Junjie-Zhong <zjj476778843@gmail.com>
2021-11-13 15:36:33 +0800

    add sleep

--- Makefile
+++ Makefile
@@ -135,6 +135,7 @@ mkfs/mkfs: mkfs/mkfs.c $K/fs.h $K/param.h
 UPROGS=\
 	$U/_cat\
 	$U/_echo\
+	$U/_sleep\
 	$U/_forktest\
 	$U/_grep\
 	$U/_init\
---
+++ user/sleep.c
@@ -0,0 +1,14 @@
+#include "user/user.h"
+#include "kernel/types.h"
+#include "kernel/stat.h"
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(2, "Error: please passing a correct paramter\n");
+        exit(-1);
+    }
+ 
+    int sleeptime = atoi(argv[1]);
+    sleep(sleeptime);
+    exit(0);
+}
